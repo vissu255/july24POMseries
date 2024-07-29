ca6fc6844124f7c8d32ac8cee0e1853c0c77f879
vissu255 <viswanathbuchi@gmail.com>
2024-07-29 08:34:00 +0530

    conflict

---
+++ src/main/java/com/opencart/pages/Conflict.java
@@ -0,0 +1,13 @@
+package com.opencart.pages;
+
+public class Conflict {
+	
+	
+	public void funA(){
+			System.out.println("Conflict");
+	}
+	public void funB(){
+		System.out.println("solved");
+	}
+
+}
--- src/main/java/com/opencart/pages/Feature2.java
+++ src/main/java/com/opencart/pages/Feature2.java
@@ -1,5 +1,9 @@
 package com.opencart.pages;
 
 public class Feature2 {
+	
+	public void funBFeature2(){
+		System.out.println("fun b of feature 2");
+	}
 
 }
